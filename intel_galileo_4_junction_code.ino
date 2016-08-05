f50eae0781e8a78147c8e45efe3c4733dcb283a5
Pallavi-mp <Pallavi-mp@users.noreply.github.com>
2016-08-05 20:41:34 +0530

    Add files via upload
    The first code is for the intel galileo, to detect the presence of an EMV and change the corresponding signal to green.
    The second code is for the linkit one microcontroller, to send values to the ubidots cloud computing platform.

---
+++ intel_galileo_4_junction_code.ino
@@ -0,0 +1,362 @@
+int s1[3]={5,6,7}; //initialising an array s1 with pin numbers 5,6,7.
+int s2[3]={2,3,4};//initialising array s2 with pin numbers 2,3,4.
+int s4[3]={8,9,10};//initialising array s3 with pin numbers 8,9,10.
+int s3[3]={11,12,13};//initialising array s4 with pin numbers 11,12,13.
+int val1,val2,val3,val4,c,i,f;//Declaring variables of integer type
+int freq=0;//initialising variable frequency of integer type to 0. This reads the output from the frequency sensing circuit.
+
+
+void interrupt1();//declaration of function interrupt1().
+void interrupt2();//declaration of function interrupt2().
+void interrupt3();//declaration of function interrupt3().
+void interrupt4();//declaration of function interrupt4().
+
+void setup() //all instructions given here will be executed once at the starting of the program.
+{
+
+  for(i=0;i<3;i++)//loop to initialise each pin.
+  {
+    pinMode(s1[i],OUTPUT);//initialising pins as OUTPUT pins.
+    pinMode(s2[i],OUTPUT);
+    pinMode(s3[i],OUTPUT);
+    pinMode(s4[i],OUTPUT);
+  } 
+  Serial.begin(9600);//Setting the baud rate to 9600.
+ 
+}
+
+void readval()//function readval() that doesnt return any type and doesnt take any arguments. It reads analog values from the analog input pins.
+{
+   val1 = analogRead(A0);//variable val1 stores the analogRead value of analog input pin A0 of the intel Galileo.
+   val2 = analogRead(A1);
+   val3 = analogRead(A2);
+   val4 = analogRead(A3);
+
+}
+
+int sensor()// function sensor() prints the analogRead values on the analog input pins on the serial monitor.
+{
+  readval();//function readval() is called.
+  Serial.print("val1: ");//command to print on the serial monitor.
+  Serial.println(val1);
+  Serial.print("val2: ");
+  Serial.println(val2);
+  Serial.print("val3: ");
+  Serial.println(val3);
+  Serial.print("val4: ");
+  Serial.println(val4);
+}
+
+
+void check()//function that checks for the presence of the ambulance. 
+{
+ if(freq>0) //if the frequency sensor gives a digital high output the following commands are executed.
+ {
+    sensor();//calling of function sensor().
+   if((val2>val1) && (val2>val3) && (val2>val4)) //checks if analogPin A1 gives the highest output
+    {
+      interrupt1();//function interrupt1() is called.
+    }
+   if((val3>val2) && (val3>val1) && (val3>val4))//checks if analogPin A1 gives the highest output
+    {
+      interrupt2();//function interrupt2() is called.
+    }
+    if((val4>val2) && (val4>val3) && (val4>val1))//checks if analogPin A1 gives the highest output
+    {
+      interrupt3();//function interrupt3() is called.
+    }
+   if((val1>val2) && (val1>val3) && (val1>val4))//checks if analogPin A1 gives the highest output
+    {
+      interrupt4();//function interrupt4() is called.
+    }
+ }
+ 
+} 
+ 
+void interrupt4()
+{
+  while(val1>val2 && val1>val3 && val1>val4)//enclosed instructions are executed as long as val1 is the greatest among val1, val2, val3 and val4.
+  {
+  digitalWrite(s1[0],HIGH);//output pin 5 is made HIGH, indicating that the red light is on.
+  digitalWrite(s1[1],LOW);//output pin 6 is made LOW, indicating that the yellow light is off.
+  digitalWrite(s1[2],LOW);//output pin 7 is made LOW, indicating that the green light is off.
+ 
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+ 
+  digitalWrite(s4[0],LOW);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],HIGH);
+  for(i=0, c=0; i<6; i++)//to ensure that discrepancies in the microphone do not affect the state of the signal till the ambulance has passed.
+  {
+  sensor();//function sensor() is called.
+  
+  if(val1<=val2 || val1<=val3 || val1<=val4)//checks if val1 is lesser than any of the other analog inputs.
+  c++;//increments variable c;
+  Serial.print("c: ");//prints c on the serial monitor.
+  Serial.println(c);}
+  if(c>6)//checks if c is greater than 6 
+    break;//the while loop is exited.
+}
+}
+
+void interrupt3()
+{
+ while(val4>val2 && val4>val3 && val4>val1)
+  {
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+ 
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+
+  digitalWrite(s3[0],LOW);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],HIGH);
+ 
+  digitalWrite(s4[0],HIGH);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],LOW);
+ for(i=0, c=0; i<6; i++)
+  {
+  sensor();
+  
+  if(val4<=val1 || val4<=val3 || val4<=val2)
+  c++;
+  
+  Serial.print("c: ");
+  Serial.println(c);}
+  if(c>6)
+    break;
+}
+}
+
+void interrupt1()
+{
+  while(val2>val1 && val2>val3 && val2>val4)
+  {
+  digitalWrite(s1[0],LOW);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],HIGH);
+ 
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+ 
+  digitalWrite(s4[0],HIGH);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],LOW);
+ for(i=0, c=0; i<6; i++)
+  {
+  sensor();
+ 
+  if(val2<=val1 || val2<=val3 || val2<=val4)
+  c++;
+  
+  Serial.print("c: ");
+  Serial.println(c);
+  if(c>6)
+    break;
+}
+}
+
+void interrupt2()
+{
+  while(val3>val2 && val3>val1 && val3>val4)
+  {
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+ 
+  digitalWrite(s2[0],LOW);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],HIGH);
+
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+ 
+  digitalWrite(s4[0],HIGH);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],LOW);
+  for(i=0, c=0; i<6; i++)
+  {
+  sensor();
+  
+  if(val3<=val2 || val3<=val1 || val3<=val4)
+  c++;
+  
+  Serial.print("c: ");
+  Serial.println(c);}
+  if(c>6)
+    break;
+}
+}
+
+void default1()//function default() to define the regular pattern of signal lights in the absence of an EMV.
+{
+  check();//function check() is called.
+  digitalWrite(s1[0],LOW);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],HIGH);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+  check();
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+  check();
+  digitalWrite(s4[0],HIGH);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],LOW);
+ for(int i=0; i<5; i++)//the function check() is called once every second to keep checking for the presence of an EMV.
+  {
+    check();
+    delay(1000);//gives a delay of 1 second in the program.
+  }
+  
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],LOW);
+  digitalWrite(s2[1],HIGH);
+  digitalWrite(s2[2],LOW);
+  check();
+  delay(1000);
+  check();
+
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],LOW);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],HIGH);
+  
+  for(int i=0; i<5; i++)
+  {
+    check();
+    delay(1000);
+  }
+  
+  
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+  check();
+  digitalWrite(s3[0],LOW);
+  digitalWrite(s3[1],HIGH);
+  digitalWrite(s3[2],LOW);
+  check();
+  delay(1000);
+
+  
+  check();
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+  check();
+  digitalWrite(s3[0],LOW);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],HIGH);
+ 
+for(int i=0; i<5; i++)
+  {
+    check();
+    delay(1000);
+  }
+ 
+  
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+ check();
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+  check();
+  digitalWrite(s4[0],LOW);
+  digitalWrite(s4[1],HIGH);
+  digitalWrite(s4[2],LOW);
+  check();
+  delay(1000);
+
+  check();
+  
+  digitalWrite(s1[0],HIGH);
+  digitalWrite(s1[1],LOW);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+  check();
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+  check();
+  digitalWrite(s4[0],LOW);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],HIGH);
+  //delay(5000);
+for(int i=0; i<5; i++)
+  {
+    check();
+    delay(1000);
+  }
+  digitalWrite(s1[0],LOW);
+  digitalWrite(s1[1],HIGH);
+  digitalWrite(s1[2],LOW);
+  check();
+  digitalWrite(s2[0],HIGH);
+  digitalWrite(s2[1],LOW);
+  digitalWrite(s2[2],LOW);
+  check();
+  digitalWrite(s3[0],HIGH);
+  digitalWrite(s3[1],LOW);
+  digitalWrite(s3[2],LOW);
+  check();
+  digitalWrite(s4[0],HIGH);
+  digitalWrite(s4[1],LOW);
+  digitalWrite(s4[2],LOW);
+  check();
+  delay(1000);
+  
+
+}
+
+
+void loop() //command in this function will be repeated continuously in a loop till the program ends.
+{
+ 
+  default1();//function default1 is called.
+ 
+}
+
+
---
+++ sending_the_value_to_Ubidots.ino
@@ -0,0 +1,128 @@
+/*
+  Ubidots WiFi client
+
+  This sketch reads an analog input and sends this value to Ubidots
+
+  Change the macro WIFI_AP, WIFI_PASSWORD, WIFI_AUTH, URL, TOKEN and VARIABLE_ID accordingly.
+
+  based on the Web Client example created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+  modified 20 Aug 2014
+  by MediaTek Inc.
+  modified 2 Sep 2015
+  by Ubidots, Inc.
+*/
+
+#include <LTask.h>//to include LTask.h header file.
+#include <LWiFi.h>//to include LWifi.h header file.
+#include <LWiFiClient.h>//to include LWifiClient.h header file.
+
+#define WIFI_AP "Pallavi"//name of wifi network the board is connected to.
+#define WIFI_PASSWORD "7032"//password of that wifi network if it is secured with WPA or WPA2.
+#define WIFI_AUTH LWIFI_WPA  // choose from LWIFI_OPEN, LWIFI_WPA, or LWIFI_WEP, based on whether it is free or secured.
+
+// Ubidots information
+
+#define URL    "things.ubidots.com"       //the URL that values will be sent to.
+#define TOKEN  "muNabx2IsIoAvktq0UnqFn8JXkPgjY"          // replace with your Ubidots token generated in your profile tab
+#define ID "579111747625425c9979d4c0"                // create a variable in Ubidots and put its ID here (http://app.ubidots.com/ubi/datasources/)
+
+
+void setup()//function that encloses instructions to be executed only once.
+{
+  LTask.begin();//function call to
+  LWiFi.begin();//function call to begin the wifi configuration.
+  Serial.begin(9600);//sets the baud rate to 9600 bits per second.
+
+  // keep retrying until connected to AP
+  Serial.println("Connecting to AP");
+  while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))//to connect to the wifi network using name and password, and to check if it connected.
+  {
+    delay(1000);//delay of 1 second
+  }
+}
+
+// I plan to use this function in the future to reconnect to WiFi if the connection drops:
+boolean wifi_status(LWifiStatus ws) {
+  switch (ws) {
+    case LWIFI_STATUS_DISABLED:// if the wifi network isn't available.
+      return false;
+      break;
+    case LWIFI_STATUS_DISCONNECTED:// if the board is disconnected from the wifi network.
+      return false;
+      break;
+    case LWIFI_STATUS_CONNECTED:// if the board is connected to the wifi network.
+      return true;
+      break;
+  }
+  return false;
+}
+
+
+void loop()//instructions given here will repeat continuously in a loop.
+{
+  Serial.println("Connecting to Ubidots...");//prints on the serial monitor.
+  LWiFiClient c;//define a variable of type LWifiClient, named c.
+  while (!c.connect(URL, 80))//while c is not connected to ubidots..try to connect to Ubidots.
+  {
+    Serial.println("Retrying to connect...");
+    delay(100);
+  }
+
+  Serial.println("Connected!");
+
+  while (1) //infinite loop.
+  {
+    LWifiStatus ws = LWiFi.status();// variable that stores the status of the wifi connection.
+    boolean status = wifi_status(ws);//boolean variable so it stores the value TRUE or FALSE depending on the wifi status.
+    if (!status) // if the board is disconnected from the wifi.
+    {
+      Serial.println("Connecting to AP");
+      while (0 == LWiFi.connect(WIFI_AP, LWiFiLoginInfo(WIFI_AUTH, WIFI_PASSWORD)))//trying to reconnect to the wifi.
+      {
+        delay(500);
+      }
+    }
+    String payload = "{\"value\":" + String(1) + "}"; //string that is sent to the cloud(Ubidots) which consists of the value to be sent along with other parts to form the pyload.
+    String le = String(payload.length());//to find the length of the payload and store it a variable le.
+
+    if (!c.connected()) {
+      while (!c.connect(URL, 80)) {
+        delay(100);
+      }
+      Serial.println("Client reconnected!");
+    }
+
+    // Build HTTP POST request
+    c.print(F("POST /api/v1.6/variables/"));//building the first part of the request .
+    c.print(ID);the variable id is used to continue the request.
+    c.println(F("/values HTTP/1.1"));//cotinuation of the requrst, version and security type are mentioned here.
+    c.println(F("User-Agent: LinKit One/1.0"));//The type of device that is connected to the cloud.
+    c.print(F("X-Auth-Token: "));//The unique token that is given too every user.
+    c.println(TOKEN);
+    c.println(F("Connection: close"));//printing the type of connection to Ubitdots.
+    c.println(F("Content-Type: application/json"));//format in which the data is being sent to the cloud.
+    c.print(F("Content-Length: "));//Length of the content.
+    c.println(le);
+    c.print(F("Host: "));//the host URL to which data is sent.
+    c.println(URL);
+    c.println();
+    c.println(payload);//Value to be sent to the cloud.
+    c.println();
+
+    int v;declaration of int v.
+    while (c.available()) {
+      v = c.read();//The value that is sent to ubidots.
+      Serial.println("v:");
+      Serial.print(v);
+      if (v < 0) {
+        Serial.println("No response.");
+        break;
+      }
+    }
+    delay(1000);//delay of one second.
+  }
+}
+
